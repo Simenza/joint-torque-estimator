338a662b1640aecd2df17e14b57dc54d68a0b463
Simenza <federico.giuliana12@gmail.com>
2025-11-26 21:11:40 +0100

    modifica dell'angolo dei giunti e del payload in tempo reale

--- URXe_manipulator/run_torque_URXe.m
+++ URXe_manipulator/run_torque_URXe.m
@@ -1,57 +1,91 @@
-%% Script interattivo calcolo coppie URXe
+%% Script dinamico calcolo coppie URXe
 clear all; clc;
 load torque_URXe.mat   % carica torque_f
 
-disp("=== Calcolo coppie di un Manipolatore URXe ===")
+disp("=== Modalità dinamica URXe ===");
 
-%% Input
+%% Costanti
 L = [0.1625 0.425 0.3922 0.1333 0.0997 0.0996];
-theta_deg = input("Inserisci gli angoli dei giunti in gradi [t1 t2 t3 t4 t5 t6]: ");
-theta = deg2rad(theta_deg);   % converti in radianti
-W = input("Inserisci le forze [Fx Fy Fz Mx My Mz]: ");
 
-%% Calcolo coppie statiche
-tau = torque_f(L, theta, W);
-disp("Coppie ai giunti = ")
-disp(tau.')
+% Configurazione iniziale
+theta_deg = [0 0 0 0 0 0];
+theta = deg2rad(theta_deg);
 
-Tbase = [ 1  0  0  0;   
-          0 -1  0  0;    
-          0  0 -1  0.1625; 
-          0  0  0  1 ];
+% Payload iniziale
+W = [0 0 0 0 0 0];   % [Fx Fy Fz Mx My Mz]
 
-d = L;
-
-L1 = Link('a',0,'d',d(1),'alpha',pi/2);
-L2 = Link('a',d(2),'d',0,'alpha',0);
-L3 = Link('a',d(3),'d',0,'alpha',0);
-L4 = Link('a',0,'d',d(4),'alpha',pi/2);
-L5 = Link('a',0,'d',d(5),'alpha',-pi/2);
-L6 = Link('a',0,'d',d(6),'alpha',0);
-
-Rob1 = SerialLink([L1 L2 L3 L4 L5 L6], 'name','URXe');
-Rob1.base = Tbase;
-
-%% Disegno iniziale
+%% Figure
 figure(1); clf; hold on;
+
+% Prima visualizzazione
+Rob1 = SerialLink( ...
+    [ Link('a',0,'d',L(1),'alpha',pi/2), ...
+      Link('a',L(2),'d',0,'alpha',0), ...
+      Link('a',L(3),'d',0,'alpha',0), ...
+      Link('a',0,'d',L(4),'alpha',pi/2), ...
+      Link('a',0,'d',L(5),'alpha',-pi/2), ...
+      Link('a',0,'d',L(6),'alpha',0) ], ...
+    'name', 'URXe');
+
+Rob1.base = [1 0 0 0; 0 -1 0 0; 0 0 -1 0.1625; 0 0 0 1];
+
 Rob1.plot(theta);
-Rob1.teach(theta);
-title("URXe - Visualizzazione coppie");
-drawnow;
+title("URXe - Coppie dinamiche in tempo reale");
 
-%% Mostra label dei giunti nell'angolo in alto a destra
-ax = gca;
-xlims = ax.XLim;
-ylims = ax.YLim;
-zlims = ax.ZLim;
+%% LOOP dinamico
+while true
 
-label_x = xlims(2);                  
-label_y = ylims(2);                  
-label_z = zlims(2);                  
+    % CHIEDI all'utente cosa vuole aggiornare
+    disp(" ");
+    disp("1) Cambia angoli dei giunti");
+    disp("2) Cambia payload (forze/momenti)");
+    disp("3) Esci");
+    scelta = input("Seleziona opzione: ");
 
-for i = 1:6
-    text(label_x, label_y, label_z - (i-1)*0.1, ...
-        sprintf('J%d: %.2f Nm', i, tau(i)), ...
-        'FontSize',12,'Color','r','FontWeight','bold', ...
-        'HorizontalAlignment','right');
+    if scelta == 3
+        break;
+    end
+
+    % --- Aggiornamento angoli ---
+    if scelta == 1
+        theta_deg = input("Inserisci nuovi angoli [t1..t6] in gradi: ");
+        theta = deg2rad(theta_deg);
+    end
+
+    % --- Aggiornamento payload ---
+    if scelta == 2
+        W = input("Inserisci forze [Fx Fy Fz Mx My Mz]: ");
+    end
+
+    %% Ricalcolo coppie statiche
+    tau = torque_f(L, theta, W);
+    disp("Coppie aggiornate (Nm):");
+    disp(tau.');
+
+    %% Aggiorna grafica robot
+    Rob1.animate(theta);
+    drawnow;
+
+    %% Cancella vecchie etichette
+    delete(findall(gca, 'Type', 'text'));
+
+    %% Scrivi nuove etichette
+    ax = gca;
+    xlims = ax.XLim;
+    ylims = ax.YLim;
+    zlims = ax.ZLim;
+
+    label_x = xlims(2);
+    label_y = ylims(2);
+    label_z = zlims(2);
+
+    for i = 1:6
+        text(label_x, label_y, label_z - (i-1)*0.1, ...
+             sprintf('J%d: %.2f Nm', i, tau(i)), ...
+             'FontSize',12,'Color','r','FontWeight','bold', ...
+             'HorizontalAlignment','right');
+    end
+
 end
+
+disp("Modalità dinamica terminata.");
